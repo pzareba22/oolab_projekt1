dd12da84d587a6b9f546d09017bd425d19c350b5
pzareba22 <pzareba22@gmail.com>
2021-12-26 12:56:43 +0100

    misc

--- src/main/java/agh/ics/oop/proj1/App.java
+++ src/main/java/agh/ics/oop/proj1/App.java
@@ -4,16 +4,13 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.util.Objects;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public class App extends Application {
     public static void main(String[] args){
-        System.out.println("Hello world");
+//        System.out.println("Hello world");
         launch(args);
     }
 
@@ -28,7 +25,7 @@ public class App extends Application {
             e.printStackTrace();
         }
 /*
-    1. pokazać ekran z ustawieniami symulacji
+    1. pokazać ekran z ustawieniami symulacji - DONE
     2. po wciśnięciu guzika na ekranie rozpocząć symulację
     3. w trakcie symulacji pokazywać wykresy
     4. po symulacji pokazać ekran z podsumowaniem

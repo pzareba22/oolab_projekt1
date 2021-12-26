203360d2393d366fed01efbbb47d202dee6fd4cd
pzareba22 <pzareba22@gmail.com>
2021-12-27 00:15:22 +0100

    changed jungle to a single rectangle (and other stuff)

--- src/main/java/agh/ics/oop/proj1/Map.java
+++ src/main/java/agh/ics/oop/proj1/Map.java
@@ -1,6 +1,13 @@
 package agh.ics.oop.proj1;
 
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class Map {
     public final int width;
@@ -11,40 +18,98 @@ public class Map {
     public final int grassEnergy;
     public final int breedingEnergy;
     public final boolean isMagical;
-    public final MapField[][] fieldsMap;
     private final GridPane gridPane;
+    public final double gridSquareSide;
+    public final int jungleX;
+    public final int jungleY;
+    public final boolean[][] foodMap;
+    public final HashMap<Vector2d, Animal> animalMap;
 
     Map(int[] parameters, boolean isMagical, GridPane gridPane){
-        height = parameters[0];
-        width = parameters[1];
-        jungleHeight = parameters[2];
-        jungleWidth = parameters[3];
-        animalNumber = parameters[4];
-        grassEnergy = parameters[5];
-        breedingEnergy = parameters[6];
+
+//      inicjalizacja pól
+        this.height = parameters[0];
+        this.width = parameters[1];
+        this.jungleHeight = parameters[2];
+        this.jungleWidth = parameters[3];
+        this.animalNumber = parameters[4];
+        this.grassEnergy = parameters[5];
+        this.breedingEnergy = parameters[6];
         this.isMagical = isMagical;
         this.gridPane = gridPane;
+        this.gridSquareSide = calculateGridSquareSide();
+        this.foodMap = new boolean[width][height];
+        this.animalMap = new HashMap<>();
 
-        this.fieldsMap = new MapField[width][height];
+
+
+
+
+//      inicjalizacja wierszy i kolumn w GridPane
         for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; j++) {
-                this.fieldsMap[i][j] = new MapField(String.valueOf(getClass().getResource("/images/lightGreen.png")), 20);
-            }
+            ColumnConstraints constraints = new ColumnConstraints();
+            constraints.setPercentWidth(100.0 / width);
+            gridPane.getColumnConstraints().add(constraints);
         }
-        this.fieldsMap[0][0] = new MapField(String.valueOf(getClass().getResource("/images/darkGreen.png")), 20);
+        for (int i = 0; i < height; i++) {
+            RowConstraints constraints = new RowConstraints();
+            constraints.setPercentHeight(100.0/ height);
+            gridPane.getRowConstraints().add(constraints);
+        }
+
+//      umieszczenie prostokąta z dżunglą
+        Rectangle rectangle = new Rectangle();
+        rectangle.setFill(javafx.scene.paint.Color.web("#1b6b05"));
+        rectangle.setWidth(jungleWidth);
+        rectangle.setHeight(jungleHeight);
+
+        this.jungleX = (width-jungleWidth) / 2;
+        this.jungleY = (height-jungleHeight) / 2;
+
+        int jungleRealWidth = (int) (gridPane.getPrefWidth() * jungleWidth / width);
+        int jungleRealHeight = (int) (gridPane.getPrefHeight() * jungleHeight / height);
+        rectangle.setWidth(jungleRealWidth);
+        rectangle.setHeight(jungleRealHeight);
+
+        gridPane.add(rectangle, this.jungleX, this.jungleY, jungleWidth, jungleHeight);
+
+
     }
 
+
     public boolean canMoveTo(Vector2d position){
-        MapElement element = fieldsMap[position.x][position.y].getTopElement();
-        return (!(element instanceof Animal));
+        return animalMap.containsKey(position);
+//        MapElement element = fieldsMap[position.x][position.y].getTopElement();
+//        return (!(element instanceof Animal));
     }
 
     public void show(){
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < height; j++) {
-                gridPane.add(fieldsMap[i][j].imageView, i, j, 1, 1);
+                Vector2d position = new Vector2d(i, j);
+                if(animalMap.containsKey(position)){
+
+                }
+//                if(fieldsMap[i][j] != null){
+//                    gridPane.add(fieldsMap[i][j].imageView, i, j, 1, 1);
+//                }
             }
         }
+        Circle circle = new Circle();
+        circle.setRadius(gridSquareSide/2);
+        circle.setFill(javafx.scene.paint.Color.web("#c22"));
+        gridPane.add(circle, 1, 0, 1, 1);
+    }
+
+    private double calculateGridSquareSide(){
+        double gridPaneWidth = gridPane.getPrefWidth();
+        double gridPaneHeight = gridPane.getPrefHeight();
+
+        double xSize = gridPaneWidth / width;
+        double ySize = gridPaneHeight / height;
+
+        return Math.min(xSize, ySize);
+
     }
 
 }

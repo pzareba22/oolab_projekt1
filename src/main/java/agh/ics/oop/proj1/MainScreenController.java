e5b50be62851f48ce2529a02c1eec37628a042e5
pzareba22 <pzareba22@gmail.com>
2021-12-26 13:21:45 +0100

    working on MainScreenController

--- src/main/java/agh/ics/oop/proj1/MainScreenController.java
+++ src/main/java/agh/ics/oop/proj1/MainScreenController.java
@@ -1,4 +1,79 @@
 package agh.ics.oop.proj1;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+
 public class MainScreenController {
+    private int mapHeight, mapWidth, jungleHeight, jungleWidth, animalNumber, grassEnergy, breedingEnegry;
+    private SimulationEngine[] engines;
+    private Thread[] engineThreads;
+    private boolean running;
+    private int simulationsNumber;
+
+    @FXML
+    private Button runButton;
+
+    @FXML
+    private void initialize(){
+        running = true;
+        runButton.setText("Stop");
+    }
+
+    public void buttonClicked(ActionEvent e){
+        if(running){
+            try {
+                stopEngines();
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+            runButton.setText("Start");
+
+        } else {
+            startEngines();
+            runButton.setText("Stop");
+        }
+        running = !running;
+    }
+
+    public void loadData(int[] parameters){
+        mapHeight = parameters[0];
+        mapWidth = parameters[1];
+        jungleHeight = parameters[2];
+        jungleWidth = parameters[3];
+        animalNumber = parameters[4];
+        grassEnergy = parameters[5];
+        breedingEnegry = parameters[6];
+    }
+
+    public void startSimulation(int simulationsNumber){
+        this.simulationsNumber = simulationsNumber;
+        engines = new SimulationEngine[simulationsNumber];
+        engineThreads = new Thread[simulationsNumber];
+        for (int i = 0; i < simulationsNumber; i++) {
+            engines[i] = new SimulationEngine("Engine " + i, 100);
+            engineThreads[i] = new Thread(engines[i]);
+
+            engineThreads[i].setDaemon(true);
+            engineThreads[i].start();
+        }
+    }
+
+    // TODO - pauzowanie i wznawianie silników
+
+    private void startEngines() {
+        for (int i = 0; i < simulationsNumber; i++) {
+            engineThreads[i].notify();
+        }
+    }
+
+    private void stopEngines() throws InterruptedException {
+        for (int i = 0; i < simulationsNumber; i++) {
+            synchronized (engineThreads[i]){
+                engineThreads[i].wait();
+            }
+        }
+
+    }
 }
